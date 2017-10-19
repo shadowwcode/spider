5143f90d5638a4cb3bdfc5c85e45f6bc55220bb4
shadow-code <15094066966@163.com>
2017-10-19 10:31:20 +0800

    .

--- practice/single_test/single_code/15day.py
+++ practice/single_test/single_code/15day.py
@@ -1,11 +1,53 @@
 # -*- coding: UTF-8 -*-
 
 
-# 53
+# 56
+if __name__ == ' ':
+    from Tkinter import *
+
+    canvas = Canvas(width=800, height=600, bg='yellow')
+    canvas.pack(expand=YES, fill=BOTH)
+    k = 1
+    j = 1
+    for i in range(0, 26):
+        canvas.create_oval(310-k, 250-k, 310+k, 250 + k, width=1)
+        k += j
+        j += 0.3
+
+    # mainloop()
+
+
+# 57
+if __name__ == '  ':
+    from Tkinter import *
+
+    canvas = Canvas(width=300, height=300, bg='green')
+    canvas.pack(expand=YES, fill=BOTH)
+    x0 = 263
+    y0 = 263
+    y1 = 275
+    x1 = 275
+    for i in range(19):
+        canvas.create_line(x0, y0, x0, y1, width=1, fill='red')
+        x0 = x0 - 5
+        y0 = y0 - 5
+        x1 = x1 + 5
+        y1 = y1 + 5
+
+    x0 = 263
+    y1 = 275
+    y0 = 263
+    for i in range(21):
+        canvas.create_line(x0, y0, x0, y1, fill='red')
+        x0 += 5
+        y0 += 5
+        y1 += 5
+
+    mainloop()
 
 
 
-
+# 61 杨辉三角形
 
 
 
