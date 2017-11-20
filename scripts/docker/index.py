4f8e327852ac9f4e3652954a429e0d88f20e4942
shadow-code <15094066966@163.com>
2017-11-20 10:13:25 +0800

    .

---
+++ scripts/docker/index.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+#authors: shadow
+#date: 2017-11-20
+
+
+import sys
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
+import socket
+import fcntl
+import struct
+import pickle
+from datetime import datetime
+from collections import OrderedDict
+
+
+class HandlerClass(SimpleHTTPRequestHandler):
+    def get_ip_address(self, ifname):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(
+            s.fileno(),
+            0x8915,
+            struct.pack('256s', ifname[:15])
+    def log_message(self, format, *args):
+        if len(args) < 3 or "200" not in args[1]:
+            return
+        try:
+            request = pickle.load(open("pickle_data.txt","r"))
+        except:
+            request=OrderedDict()
+        time_now = datetime.now()
+        ts = time_now.strftime('%Y-%m-%d %H:%M:%S')
+        server = self.get_ip_address('eth0')
+        host=self.address_string()
+        addr_pair = (host,server)
+        if addr_pair not in request:
+            request[addr_pair]=[1,ts]
+        else:
+            num = request[addr_pair][0]+1
+            del request[addr_pair]
+            request[addr_pair]=[num,ts]
+        file=open("index.html", "w")
+        file.write("<!DOCTYPE html> <html> <body><center><h1><font color=\"blue\" face \
+        =\"Georgia, Arial\" size=8><em>HA</em></font> Webpage Visit Results</h1></center>")
+        for pair in request:
+            if pair[0] == host:
+                guest = "LOCAL: "+pair[0]
+            else:
+                guest = pair[0]
+                    )[20:24])
+
+
+
+
+
+
+
+
+
+
+
